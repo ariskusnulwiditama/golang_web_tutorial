b5401ef82afb09af85d079a3056b95e868c363f0
ariskusnulwiditama <ariskusnul@gmail.com>
2021-06-13 11:25:05 +0700

    add file cookies test

--- cookie_test.go
+++ cookie_test.go
@@ -17,6 +17,7 @@ func SetCookie(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "success create cookie")
 }
 
+
 func GetCookie(writer http.ResponseWriter, request *http.Request) {
 	cookie, err := request.Cookie("X-PZN-Name")
 	if err != nil {
